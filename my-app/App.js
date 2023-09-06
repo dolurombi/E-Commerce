5cc4fbf2c3757a8d2287d69c1ce2e88ace27dfc6
dolurombi <dolurombi@gmail.com>
2023-09-06 19:50:25 +0100

    updated fronts

--- my-app/App.js
+++ my-app/App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from 'react';
+
 
 export default function App() {
+
+  const [fontsloded]= useFonts({
+    regular: require("../my-app/assets/fonts/Poppins-Regular.ttf"),
+    blod: require("../my-app/assets/fonts/Poppins-Blod.ttf"),
+    extrabold: require("../my-app/assets/fonts/Poppins-ExtraBold.ttf"),
+    light: require("../my-app/assets/fonts/Poppins-Light.ttf"),
+    medium: require("../my-app/assets/fonts/Poppins-Medium.ttf"),
+    semibold: require("../my-app/assets/fonts/Poppins-SemiBold.ttf"),
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
